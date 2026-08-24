4b3eae41e09c97dcea80b7f70b8411159176f893
tazlin <tazlin.on.github@gmail.com>
2026-08-23 21:01:07 -0400

    fix: update compare to script to v8

--- scripts/compare_pr_to_main.py
+++ scripts/compare_pr_to_main.py
@@ -1,12 +1,10 @@
 import argparse
 import json
-
 from pathlib import Path
 
-from horde_model_reference.legacy.classes.raw_legacy_model_database_records import (
-    RawLegacy_StableDiffusion_ModelReference,
-    RawLegacy_FileRecord,
-    RawLegacy_StableDiffusion_ModelRecord,
+from horde_model_reference.legacy.classes.legacy_models import (
+    LegacyConfigFile,
+    LegacyStableDiffusionRecord,
 )
 
 
@@ -14,56 +12,49 @@ def compare_pr_to_main(
     pr_path: Path,
     main_path: Path,
 ) -> tuple[
-    dict[str, RawLegacy_StableDiffusion_ModelRecord],
-    dict[str, RawLegacy_StableDiffusion_ModelRecord],
-    dict[str, RawLegacy_StableDiffusion_ModelRecord],
+    dict[str, LegacyStableDiffusionRecord],
+    dict[str, LegacyStableDiffusionRecord],
+    dict[str, LegacyStableDiffusionRecord],
 ]:
     with open(main_path) as main_sd_model_database_file:
         main_sd_model_database_file_contents = main_sd_model_database_file.read()
 
     main_sd_model_database_file_json = json.loads(main_sd_model_database_file_contents)
-    main_model_reference = RawLegacy_StableDiffusion_ModelReference(
-        root={
-            k: RawLegacy_StableDiffusion_ModelRecord.model_validate(v)
-            for k, v in main_sd_model_database_file_json.items()
-        },
-    )
+    main_model_reference = {
+        k: LegacyStableDiffusionRecord.model_validate(v)
+        for k, v in main_sd_model_database_file_json.items()
+    }
 
     with open(pr_path) as pr_sd_model_database_file:
         pr_sd_model_database_file_contents = pr_sd_model_database_file.read()
 
     pr_sd_model_database_file_json = json.loads(pr_sd_model_database_file_contents)
-    pr_model_reference = RawLegacy_StableDiffusion_ModelReference(
-        root={
-            k: RawLegacy_StableDiffusion_ModelRecord.model_validate(v)
-            for k, v in pr_sd_model_database_file_json.items()
-        },
-    )
+    pr_model_reference = {
+        k: LegacyStableDiffusionRecord.model_validate(v)
+        for k, v in pr_sd_model_database_file_json.items()
+    }
 
-    models_added = {}
-    models_removed = {}
-    models_changed = {}
+    models_added: dict[str, LegacyStableDiffusionRecord] = {}
+    models_removed: dict[str, LegacyStableDiffusionRecord] = {}
+    models_changed: dict[str, LegacyStableDiffusionRecord] = {}
     models_changed_hashes: dict[str, tuple[str | None, str | None]] = {}
 
-    for model_name, model in pr_model_reference.root.items():
-        if model_name not in main_model_reference.root:
+    for model_name, model in pr_model_reference.items():
+        if model_name not in main_model_reference:
             models_added[model_name] = model
         else:
             pr_hash = None
-            for _, pr_records in model.config.items():
+            for _, pr_records in model.config.files:
                 for pr_record in pr_records:
-                    if (
-                        isinstance(pr_record, RawLegacy_FileRecord)
-                        and pr_record.sha256sum
-                    ):
+                    if isinstance(pr_record, LegacyConfigFile) and pr_record.sha256sum:
                         pr_hash = pr_record.sha256sum
                         break
 
             main_hash = None
-            for _, main_records in main_model_reference.root[model_name].config.items():
+            for _, main_records in main_model_reference[model_name].config.files:
                 for main_record in main_records:
                     if (
-                        isinstance(main_record, RawLegacy_FileRecord)
+                        isinstance(main_record, LegacyConfigFile)
                         and main_record.sha256sum
                     ):
                         main_hash = main_record.sha256sum
@@ -73,17 +64,17 @@ def compare_pr_to_main(
                 models_changed[model_name] = model
                 models_changed_hashes[model_name] = (main_hash, pr_hash)
 
-    for model_name, model in main_model_reference.root.items():
-        if model_name not in pr_model_reference.root:
+    for model_name, model in main_model_reference.items():
+        if model_name not in pr_model_reference:
             models_removed[model_name] = model
 
     return models_added, models_removed, models_changed
 
 
 def write_changes_to_dir(
-    models_added: dict[str, RawLegacy_StableDiffusion_ModelRecord],
-    models_removed: dict[str, RawLegacy_StableDiffusion_ModelRecord],
-    models_changed: dict[str, RawLegacy_StableDiffusion_ModelRecord],
+    models_added: dict[str, LegacyStableDiffusionRecord],
+    models_removed: dict[str, LegacyStableDiffusionRecord],
+    models_changed: dict[str, LegacyStableDiffusionRecord],
     output_dir: Path,
     pr_hash: str,
     main_hash: str,
@@ -147,7 +138,9 @@ def main():
     else:
         output = f"Models added from {hash_compared}:\n"
         for model_name, model in models_added.items():
-            fixed_model_version = model.version.removeprefix("v")
+            fixed_model_version = (
+                model.version.removeprefix("v") if model.version else "N/A"
+            )
             output += f"  + {model_name} v{fixed_model_version} ({model.baseline})\n"
             output += f"    {model.description}\n"
             output += f"    nsfw: {model.nsfw}\n"
